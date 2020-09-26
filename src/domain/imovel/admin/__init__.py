292047dadbbb1c3a8aa3dd43440899c942c46621
CesarCarriel <cesarcarrieldev@gmail.com>
2020-09-26 10:46:10 -0400

    ADMIN feat: create admin for tipo and propriedade

--- src/domain/imovel/admin/__init__.py
+++ src/domain/imovel/admin/__init__.py
@@ -1,1 +1,14 @@
 from django.contrib import admin
+from domain.imovel.models import Tipo, Propriedade;
+
+
+class TipoAdmin(admin.ModelAdmin):
+    list_display = ['nome']
+
+
+class PropriedadeAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'valor', 'tipo', 'foto']
+
+
+admin.site.register(Tipo, TipoAdmin)
+admin.site.register(Propriedade, PropriedadeAdmin)
--- src/domain/imovel/models/tipo.py
+++ src/domain/imovel/models/tipo.py
@@ -3,3 +3,6 @@ from django.db import models
 
 class Tipo(models.Model):
     nome = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nome
