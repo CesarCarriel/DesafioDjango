d395df4df07d4300a4ba223269686c2e0800e219
CesarCarriel <cesarcarrieldev@gmail.com>
2020-09-26 14:04:50 -0400

    ADMIN feat: import_export configuration

--- src/project/settings.py
+++ src/project/settings.py
@@ -21,7 +21,6 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -34,7 +33,6 @@ ALLOWED_HOSTS = [
     '*'
 ]
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -46,7 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'import_export',
 
+    # Apps
     'domain.imovel.apps.ImovelConfig',
 ]
 
@@ -81,14 +81,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
     'default': env.db('DB_DEFAULT', default=f'psql://app:app@localhost:54321/app')
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -108,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -119,7 +116,6 @@ TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
@@ -136,3 +132,6 @@ MEDIA_URL = '/static/media/'
 STATICFILES_DIRS = (
     PUBLIC_DIR / 'geral',
 )
+
+# Configuration import-export
+IMPORT_EXPORT_USE_TRANSACTIONS = True
