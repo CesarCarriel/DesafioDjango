6e23c59d181d23c219129c8032f37931eb16f21e
Unknown <unknown@example.com>
2020-09-29 22:15:31 -0400

    LOCALIZACAO feat: localizacao api url configuration

--- src/project/urls.py
+++ src/project/urls.py
@@ -1,22 +1,22 @@
-"""project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from views.api.localizacao import RegiaoViewSet, RegiaoCreateViewSet, CidadeViewSet, CidadeCreateViewSet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cidade/', CidadeCreateViewSet.as_view()),
+    path('cidade/<int:pk>', CidadeViewSet.as_view()),
+    path('regiao/', RegiaoCreateViewSet.as_view()),
+    path('regiao/<int:pk>', RegiaoViewSet.as_view()),
 ]
+'''
+from core import views
+
+urlpatterns = [
+    path('persons/', views.PersonList.as_view()),
+    path('persons/<int:pk>/', views.PersonDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+
+'''
