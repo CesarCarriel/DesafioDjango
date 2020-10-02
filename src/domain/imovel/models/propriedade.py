ffc3b5c233b84de3a377f8dc119b86366c678103
CesarCarriel <cesarcarrieldev@gmail.com>
2020-10-01 20:34:32 -0400

    GEOLOCALIZACAO feat: adding field posicao

--- src/domain/imovel/models/propriedade.py
+++ src/domain/imovel/models/propriedade.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.contrib.gis.db import models
 
 
 class Propriedade(models.Model):
@@ -6,5 +6,6 @@ class Propriedade(models.Model):
     valor = models.DecimalField(decimal_places=2, max_digits=12)
     tipo = models.ForeignKey('imovel.Tipo', on_delete=models.PROTECT)
     foto = models.FileField()
+    posicao = models.PointField('posicao', srid=4326, null=True)
     criado_em = models.DateTimeField(auto_now_add=True)
     modificado_em = models.DateTimeField(auto_now=True)
